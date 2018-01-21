98ab6ab8cf286b6731877ed64219a57a6992892b
Sathvik777 <katam.sathvik@gmail.com>
2018-01-21 09:14:44 +0100

    filtering data

--- exploer_dataset.py
+++ exploer_dataset.py
@@ -20,3 +20,16 @@ def find_parent_data(parent_id):
     except Exception as e:
         #print(str(e))
         return False
+
+
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data == '[deleted]':
+        return False
+    elif data == '[removed]':
+        return False
+    else:
+        return True
